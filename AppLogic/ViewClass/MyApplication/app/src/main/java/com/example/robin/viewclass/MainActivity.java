72652fd2524a323fa4e0ce069debf3218c96a263
robin <miller.robin91@gmail.com>
2017-03-31 13:47:04 +0200

    added the basic logic for drawing the triangle , working in this stage!

--- AppLogic/ViewClass/MyApplication/app/src/main/java/com/example/robin/viewclass/MainActivity.java
+++ AppLogic/ViewClass/MyApplication/app/src/main/java/com/example/robin/viewclass/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.robin.viewclass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.robin.viewclass.views.*;
 
+/**
+ * basic class to call the new view
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ViewLightUp viewLightUp = new ViewLightUp(this);
+
+        //set my created view in the App!
+        setContentView(viewLightUp);
+
+        //not needed i set my own view here!
+       // setContentView(R.layout.activity_main);
     }
 }
