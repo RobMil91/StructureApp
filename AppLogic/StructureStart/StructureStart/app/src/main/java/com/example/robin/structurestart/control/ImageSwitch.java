439284a0373141a4db2b0e180476a91d56311c78
robin <miller.robin91@gmail.com>
2017-04-01 19:46:12 +0200

    added two new classes initiated model class with getters

---
+++ AppLogic/StructureStart/StructureStart/app/src/main/java/com/example/robin/structurestart/control/ImageSwitch.java
@@ -0,0 +1,8 @@
+package com.example.robin.structurestart.control;
+
+/**
+ * Created by robin on 01.04.17.
+ */
+
+public class ImageSwitch {
+}
---
+++ AppLogic/StructureStart/StructureStart/app/src/main/java/com/example/robin/structurestart/model/Model.java
@@ -0,0 +1,43 @@
+package com.example.robin.structurestart.model;
+
+import android.graphics.Color;
+import android.view.View;
+
+/**
+ * Class to contain all the different view classes
+ */
+
+public class Model {
+
+    private Color backgroundColor;
+    private  Color lineColor;
+    private  Color triangleColor;
+    private   Color triangleLightColor;
+    private   Boolean triangleFilled;
+
+    public Model (Color backgroundColor, Color lineColor, Color triangleColor, Color triangleLightColor, Boolean triangleFilled) {
+
+
+    }
+
+
+
+    public View getImageEmpty() {
+        return null;
+    }
+
+    public View getTriangleUp() {
+        return null;
+    }
+
+    public View getTriangleLightUp() {
+        return null;
+    }
+
+    public View getTriangleDown() {
+        return null;
+    }
+    public View getTriangleLightDown() {
+        return null;
+    }
+}
