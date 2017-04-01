2e22b2b247e5d63bbd46a60b3a0fa01028e8a265
robin <miller.robin91@gmail.com>
2017-04-01 10:29:32 +0200

    added the runactivity to the structureTest working checkpoint

--- AppLogic/StructureStart/StructureStart/app/src/main/java/com/example/robin/structurestart/view/MainActivity.java
+++ AppLogic/StructureStart/StructureStart/app/src/main/java/com/example/robin/structurestart/view/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.robin.structurestart.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //create intent for the RunActivity
+        Intent intent = new Intent(this, RunActivity.class);
+
+        //start the run activity
+        startActivity(intent);
 
     }
 }
