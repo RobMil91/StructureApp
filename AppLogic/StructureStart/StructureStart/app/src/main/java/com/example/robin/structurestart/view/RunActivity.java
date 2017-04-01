df44d00b2678a15ec5def7c59991658c702c4622
robin <miller.robin91@gmail.com>
2017-04-01 10:31:20 +0200

    added the viewclass to the runactivity for test

--- AppLogic/StructureStart/StructureStart/app/src/main/java/com/example/robin/structurestart/view/RunActivity.java
+++ AppLogic/StructureStart/StructureStart/app/src/main/java/com/example/robin/structurestart/view/RunActivity.java
@@ -8,13 +8,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.example.robin.structurestart.R;
+import com.example.robin.structurestart.model.viewclasses.ViewLightUp;
 
 public class RunActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_run);
+        ViewLightUp viewLightUp = new ViewLightUp(this);
+        setContentView(viewLightUp);
 
     }
 
