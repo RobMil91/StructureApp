ac59b40b28158fe799670392964d22816ce853ec
robin <miller.robin91@gmail.com>
2017-04-01 10:22:23 +0200

    added a structure test project for the end logic into Applogic

---
+++ AppLogic/StructureStart/StructureStart/app/src/main/java/com/example/robin/structurestart/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.robin.structurestart.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.robin.structurestart.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
