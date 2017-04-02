26ff1be3e6b905e0f92211aeee9c7da1b4ac1e96
robin <miller.robin91@gmail.com>
2017-04-02 19:42:25 +0200

    added the algorithm class structure added some imageswitch code

--- AppLogic/StructureStart/StructureStart/app/src/main/java/com/example/robin/structurestart/control/ImageSwitch.java
+++ AppLogic/StructureStart/StructureStart/app/src/main/java/com/example/robin/structurestart/control/ImageSwitch.java
@@ -1,12 +1,47 @@
 package com.example.robin.structurestart.control;
 
+import android.view.View;
+
+import com.example.robin.structurestart.model.Model;
+
 import java.util.logging.Handler;
 
 /**
  * Class to switch the images
  */
 
-public class ImageSwitch {
+public class ImageSwitch extends View {
 
-    Handler handler;
+   private android.os.Handler handler;
+    private Model model;
+    private int entiretime;
+    private int lightUpTime;
+    private int pausetime;
+    private boolean sound;
+    private int soundKind;
+
+    /**
+     * @param model give the switcher the model for the images
+     * @param entiretime runtime of the image sequenze
+     * @param lightUpTime time how long the lightupTriangle should be shown
+     * @param pausetime time between the the triangles
+     * @param sound if sound should be integrated boolean
+     * @param soundKind decide which kind of sound should be played
+     */
+    public ImageSwitch(Model model, int entiretime, int lightUpTime, int pausetime, boolean sound, int soundKind) {
+
+        super(this);
+
+    }
+
+
+
+    public void seqeunzstart() {
+
+        //start the handler
+        this.handler = new android.os.Handler();
+
+
+
+    }
 }
---
+++ AppLogic/StructureStart/StructureStart/app/src/main/java/com/example/robin/structurestart/model/Algorithm.java
@@ -0,0 +1,9 @@
+package com.example.robin.structurestart.model;
+
+/**
+ * Class to create an Boolean Array for the up and down
+ * which should be randomizsed
+ */
+
+public class Algorithm {
+}
